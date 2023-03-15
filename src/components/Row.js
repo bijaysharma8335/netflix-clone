5ca8bd5277330f96bb19f5b348037452a5a5d064
Bijay Sharma <bijaysharma8335@gmail.com>
2023-03-15 23:06:56 +0530

    install dependencies

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import YouTube from "react-youtube";
+import { movieTrailer } from "movie-trailer";
 import axios from "../axios";
 import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original/";
 const Row = ({ title, fetchUrl, isLargeRow }) => {
     const [movies, setMovies] = useState([]);
-const [trailerUrl, settrailerUrl] = useState('')
+    const [trailerUrl, setTrailerUrl] = useState("");
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
@@ -18,23 +19,41 @@ const [trailerUrl, settrailerUrl] = useState('')
     const opts = {
         height: "390",
         width: "100%",
-        playerVars: { // https://developers.google.com/youtube/player_parameters
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
             autoplay: 1,
         },
     };
 
-    const handleClick=(movie)=>{
-        if(trailerUrl){
-            settrailerUrl('');
+    const handleClick = (movie) => {
+        console.log("movie", movie, trailerUrl);
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            console.log("hello");
+            // movieTrailer(null, { tmdbId: movie.id })
+            //     .then((url) => {
+            //         console.log("url is " + url);
+            //         const urlParams = new URLSearchParams(new URL(url).search);
+            //         console.log("urlParams" + urlParams);
+            //         setTrailerUrl(urlParams.get("v"));
+            //     })
+            movieTrailer((movie ? movie.name : null) || (movie ? movie.title : null) || "")
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    console.log(urlParams)
+                    setTrailerUrl(urlParams.get("v"));
+                })
+                .catch((error) => console.log(error));
+            // movieTrailer(null, { tmdbId: movie.id })
+            //     .then((url) => {
+            //         console.log("url", url);
+            //         const urlParams = new URLSearchParams(new URL(url).search);
+            //         settrailerUrl(urlParams.get("v"));
+            //     })
+            //     .catch((error) => console.log(error));
         }
-        else{
-            movieTrailer(movie?.name||'').then((url)=>{
-                const urlParams=new URLSearchParams(new URL(url).search)
-
-            }).catch(error=>console.log(error))
-        }
-
-    }
+    };
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -42,19 +61,22 @@ const [trailerUrl, settrailerUrl] = useState('')
                 {/* row__posters */}
                 {movies.map((movie) => (
                     <img
-                        key={movie.id}onClick={()=>handleClick(movie)}
+                        key={movie.id}
+                        onClick={() => handleClick(movie)}
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                         alt={movie.name}
                     />
                 ))}
             </div>
-            {trailerUrl && <YouTube 
+            {trailerUrl && (
+                <YouTube
+                    // videoId='2g811Eo7K8U'
 
-            // videoId='2g811Eo7K8U'
-
-          videoId={trailerUrl}
-             opts={opts} />}
+                    videoId={trailerUrl}
+                    opts={opts}
+                />
+            )}
         </div>
     );
 };
